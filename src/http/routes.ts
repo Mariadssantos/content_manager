e18ff8d37c65d87d75d9490436e61c1b30cb3e82
Mariadssantos <mariaeduardasanntoss@gmail.com>
2024-10-29 23:15:45 -0300

    route list user

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { CreateUserController } from "../modules/User/CreateUserController";
+import { CreateUserController } from "../modules/User/CreateUser/CreateUserController";
+import { ListUserController } from "../modules/User/GetUser/ListUserController";
 
 const routes = Router();
 const createUserController = new CreateUserController();
+const listUserController = new ListUserController();
+
 
 routes.post("/user/", createUserController.handle);
+
+routes.get("/user/", listUserController.handle);
+routes.get("/user/:id", listUserController.handle);
 
 export {routes};
